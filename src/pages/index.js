ca24349ae010a9bc16386f7d899bbe0f90eab033
jasonliang512 <jasonliang512@gmail.com>
2020-02-08 10:09:17 -0800

    update index page

--- src/pages/index.js
+++ src/pages/index.js
@@ -3,13 +3,18 @@ import React from "react"
 import Layout from "../components/layout"
 import SEO from "../components/seo"
 import "./index.css"
+import carSvg from "../images/car.svg"
+import parkSvg from "../images/park.svg"
 
 const IndexPage = () => (
   <Layout>
     <SEO title="Home" />
-    <h1 className="home-header">
-      Hello World
-    </h1>
+    <div className="home-content">
+      <div className="home-images-container flex">
+        <img className="home-small-image" alt="home" src={carSvg} />
+        <img className="home-small-image" alt="park" src={parkSvg} />
+      </div>
+    </div>
   </Layout>
 )
 
