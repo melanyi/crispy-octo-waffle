7f0e37e5f64ee342f0efe9dd043550e0f329abf1
Christina <xza172@sfu.ca>
2020-02-08 19:02:02 -0800

    Final versioin of weather lists

--- src/pages/city-list.js
+++ src/pages/city-list.js
@@ -1,76 +1,55 @@
 import React from "react"
 
 import Layout from "../components/layout"
+
 import "./city-list.css"
+import axios from "axios"
 import weatherBackground from "../images/weather-background.jpg"
 
-const WeatherPage = () => {
+const IndexPage = () => {
+
+  var place = ["Vancouver", "Burnaby", "Surrey", "Langley", "Abbotsford", "Chilliwack", "Hope", "Kamloops", "Banff"];
+  const [arr, setArr] = React.useState([])
+  const weatherApiKey = "d9f7da9062592df9c2dea640b3f25f27";
+  const mapsApiKey = "AIzaSyBXk6lGDtJyh1GIfe71spYt9vvKe0bnwbw";
   React.useEffect(() => {
-    var acc = document.getElementsByClassName("accordion");
-    var i;
-    for (i = 0; i < acc.length; i++) {
-      acc[i].addEventListener("click", function() {
-        this.classList.toggle("active");
-        var panel = this.nextElementSibling;
-        if (panel.style.display === "block") {
-          panel.style.display = "none";
-        } else {
-          panel.style.display = "block";
-        }
-      });
-    }
-    
-    var cityName = ["Vancouver","Burnaby", "Surrey","Langley","Abbotsford","Chilliwack","Hope","Kamloops","Banff"];
-    var j;
-    var body = document.getElementsByTagName('body')[0];
-    
-    var key = "77b9bd2bc907dd5839c897abfe8ebd3e";
-    var i;
-    var cityTemp = [];
-    for(i = 0; i < cityName.length; i++){
-      const link = "https://api.openweathermap.org/data/2.5/weather?q="+cityName[i]+"&units=metric&apikey="+key;
-      var request = new XMLHttpRequest();
-      request.open('GET',link,true);
-      request.onload = function(){
-        var obj = JSON.parse(this.response);
-        if (request.status >= 200 && request.status < 400) {
-          var temp = obj.main.temp;
-          cityTemp.push(temp);
-        }
-        else{
-          console.log("The city doesn't exist! Kindly check");
-        } 
-      }
-    }
-    
-    for (j = 0; j < cityName.length; j++){
-      var button = document.createElement('button');
-      button.className = 'accordion'
-      button.innerHTML = cityName[j];
-      body.appendChild(button); 
-      var h = document.createElement("H1")    
-      var info = document.createTextNode(cityTemp[j]);
-    }
-    
-    request.send(); 
-    
-    return () => {
-      document.querySelectorAll("button.accordion").forEach(button => button.remove())
-    }
+    place.map(city => {
+      const start = axios(
+        `https://api.openweathermap.org/data/2.5/weather?q=${city},BC,CA&appid=${weatherApiKey}`
+      )
+        .then(response => response.data)
+        .then(data => {
+          var temp = Math.round((data.main.temp - 273.15));
+          console.log(temp);
+          setArr(prev => [...prev, { temp: temp, icon: data.weather[0].icon, name: data.name }])
+
+        })
+    })
   }, [])
-  
+
+  console.log("len", arr.length);
   return (
     <Layout>
     <div class="title">
-      <img src={weatherBackground} width="100%" height="70%" />
+      <img src={weatherBackground} alt="Beautiful road with sunshine" width="100%" height="70%" />
     </div>
-    <div class="words"><h2>Weather <br/> &emsp; Forecast</h2>
+    <div class="words"><h2>Weather <br /> &emsp; Forecast</h2>
     </div>
-    <div class="panel">
-      <p>temp</p>
-    </div>
-  </Layout>
-  )
+  
+    <table>
+      {arr.map(data => (
+        <tr>
+          <img src={`http://openweathermap.org/img/wn/${data.icon}@2x.png`} />
+          <div class="weather-info">
+            <p class="weather-num">{data.temp}&#8451;</p>
+            <p class="city-name"> {data.name}</p>
+          </div>
+        </tr>
+      ))}
+    </table>
+ 
+    </Layout>
+ )
 }
 
-export default WeatherPage
+export default IndexPage
