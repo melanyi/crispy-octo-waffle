fe5bef62657ad49e05d0862ae008cc1b623c4353
Melody Lan <melody_lan@sfu.ca>
2020-02-08 17:58:25 -0800

    Modify index page layout

--- src/pages/city-list.js
+++ src/pages/city-list.js
@@ -9,7 +9,7 @@ const WeatherPage = () => {
     var acc = document.getElementsByClassName("accordion");
     var i;
     for (i = 0; i < acc.length; i++) {
-      acc[i].addEventListener("click", function() {
+      acc[i].addEventListener("click", function () {
         this.classList.toggle("active");
         var panel = this.nextElementSibling;
         if (panel.style.display === "block") {
@@ -19,57 +19,60 @@ const WeatherPage = () => {
         }
       });
     }
-    
-    var cityName = ["Vancouver","Burnaby", "Surrey","Langley","Abbotsford","Chilliwack","Hope","Kamloops","Banff"];
+
+    var cityName = ["Vancouver", "Burnaby", "Surrey", "Langley", "Abbotsford", "Chilliwack", "Hope", "Kamloops", "Banff"];
     var j;
     var body = document.getElementsByTagName('body')[0];
-    
+
     var key = "77b9bd2bc907dd5839c897abfe8ebd3e";
     var i;
     var cityTemp = [];
-    for(i = 0; i < cityName.length; i++){
-      const link = "https://api.openweathermap.org/data/2.5/weather?q="+cityName[i]+"&units=metric&apikey="+key;
+    for (i = 0; i < cityName.length; i++) {
+      const link = "https://api.openweathermap.org/data/2.5/weather?q=" + cityName[i] + "&units=metric&apikey=" + key;
       var request = new XMLHttpRequest();
-      request.open('GET',link,true);
-      request.onload = function(){
+      request.open('GET', link, true);
+      request.onload = function () {
         var obj = JSON.parse(this.response);
         if (request.status >= 200 && request.status < 400) {
           var temp = obj.main.temp;
           cityTemp.push(temp);
         }
-        else{
+        else {
           console.log("The city doesn't exist! Kindly check");
-        } 
+        }
       }
     }
-    
-    for (j = 0; j < cityName.length; j++){
+
+    for (j = 0; j < cityName.length; j++) {
       var button = document.createElement('button');
       button.className = 'accordion'
       button.innerHTML = cityName[j];
-      body.appendChild(button); 
-      var h = document.createElement("H1")    
+      body.appendChild(button);
+      var h = document.createElement("H1")
       var info = document.createTextNode(cityTemp[j]);
     }
-    
-    request.send(); 
-    
+
+    request.send();
+
     return () => {
       document.querySelectorAll("button.accordion").forEach(button => button.remove())
     }
   }, [])
-  
+
   return (
     <Layout>
-    <div class="title">
-      <img src={weatherBackground} width="100%" height="70%" />
-    </div>
-    <div class="words"><h2>Weather <br/> &emsp; Forecast</h2>
-    </div>
-    <div class="panel">
-      <p>temp</p>
-    </div>
-  </Layout>
+      <div class="title">
+        <div class="navbar">
+          <a class="active" href="/"><i class="fa fa-fw fa-home"></i> Home</a>
+        </div>
+        <img src={weatherBackground} width="100%" height="70%" />
+      </div>
+      <div class="words"><h2>Weather <br /> &emsp; Forecast</h2>
+      </div>
+      <div class="panel">
+        <p>temp</p>
+      </div>
+    </Layout>
   )
 }
 
--- src/pages/index.js
+++ src/pages/index.js
@@ -8,13 +8,17 @@ const IndexPage = () => (
   <Layout>
     <SEO title="Home" />
     <div className="home-content">
-      <div className="home-images-container">
+      <div className="home-images-container" >
+        <div class="navbar">
+          <a class="active" href="/"><i class="fa fa-fw fa-home"></i> Home</a>
+        </div>
+
         <div className="road-image">
-          <a href = "/map">
-          <button className = "go-button">Go!</button>
+          <a href="/map">
+            <button className="go-button">Go!</button>
           </a>
           <input className="origin-input" type="text" name="origin"></input>
-          <input className="dest-input" type="text" name="destination"></input>          
+          <input className="dest-input" type="text" name="destination"></input>
         </div>
       </div>
     </div>
